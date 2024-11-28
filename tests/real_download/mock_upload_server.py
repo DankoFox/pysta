1e4d9ea625568baa8f6fd3cff6e661eeddc20dc8
DankChoir <timzek7@gmail.com>
2024-11-28 13:56:57 +0700

    [Fix Real Test]: add path for the test

--- tests/real_download/mock_upload_server.py
+++ tests/real_download/mock_upload_server.py
@@ -1,3 +1,8 @@
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../")))
+
 from node.upload_manager import UploadManager
 from node.file_manager import FileManager
 import threading
